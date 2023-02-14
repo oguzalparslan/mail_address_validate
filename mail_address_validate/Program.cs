e36aefd25521572828ca5d6d4308b068264c9f3b
oguzalparsln@icloud.com <oguzalparsln@icloud.com>
2023-02-14 23:28:35 +0300

    Add project files.

---
+++ mail_address_validate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Mail;
+
+static bool IsValidEmail(string email)
+{
+    try
+    {
+        MailAddress mailAddress = new MailAddress(email);
+        return true;
+    }
+    catch (FormatException)
+    {
+        return false;
+    }
+}
+
+
+string email = "oguzalparsln@icloud.com";
+bool isValid = IsValidEmail(email);
+if (isValid)
+{
+    Console.WriteLine("E-posta adresi geçerli.");
+    Console.WriteLine("E-posta : " + email);
+}
+else
+{
+    Console.WriteLine("E-posta adresi ({0}) geçerli değil.", email);
+}
